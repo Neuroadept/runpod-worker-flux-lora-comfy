8bc78f5a3d8899ea71e93daa010bafeec8fd5725
@aydkhuzin@ozon.ru <aydkhuzin@ozon.ru>
2025-03-02 01:00:45 +0300

    release 1.0.7

--- src/start.sh
+++ src/start.sh
@@ -5,16 +5,16 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
 # Serve the API and don't shutdown the container
-if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+#if [ "$SERVE_API_LOCALLY" == "true" ]; then
+#    echo "runpod-worker-comfy: Starting ComfyUI"
+#    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+#
+#    echo "runpod-worker-comfy: Starting RunPod Handler"
+#    python3 -u /src/rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
+#else
+  echo "runpod-worker-comfy: Starting ComfyUI"
+  python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
 
-    echo "runpod-worker-comfy: Starting RunPod Handler"
-    python3 -u /src/rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
-else
-    echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
-
-    echo "runpod-worker-comfy: Starting RunPod Handler"
-    python3 -u /src/rp_handler.py
-fi
+  echo "runpod-worker-comfy: Starting RunPod Handler"
+  python3 -u /src/rp_handler.py
+#fi
